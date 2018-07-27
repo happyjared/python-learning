47188d261895f0f67842ba9ca5425125778737a6
jared <dong1013@foxmail.com>
2018-07-27 17:02:32 +0800

    add

--- miscellaneous/boss-show.py
+++ miscellaneous/boss-show.py
@@ -1,6 +1,6 @@
 import csv
 import json
-
+import random
 import requests
 from bs4 import BeautifulSoup
 
@@ -10,15 +10,36 @@ class Spider(object):
     def __init__(self) -> None:
         self.list_data = []
         self.list_repeat = []
+        self.user_agent_list = [
+            "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/22.0.1207.1 Safari/537.1",
+            "Mozilla/5.0 (X11; CrOS i686 2268.111.0) AppleWebKit/536.11 (KHTML, like Gecko) Chrome/20.0.1132.57 Safari/536.11",
+            "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.6 (KHTML, like Gecko) Chrome/20.0.1092.0 Safari/536.6",
+            "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.6 (KHTML, like Gecko) Chrome/20.0.1090.0 Safari/536.6",
+            "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/19.77.34.5 Safari/537.1",
+            "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.9 Safari/536.5",
+            "Mozilla/5.0 (Windows NT 6.0) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.36 Safari/536.5",
+            "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3",
+            "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3",
+            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_0) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3",
+            "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1062.0 Safari/536.3",
+            "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1062.0 Safari/536.3",
+            "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",
+            "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",
+            "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",
+            "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.0 Safari/536.3",
+            "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/535.24 (KHTML, like Gecko) Chrome/19.0.1055.1 Safari/535.24",
+            "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/535.24 (KHTML, like Gecko) Chrome/19.0.1055.1 Safari/535.24",
+            'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36',
+        ]
 
     def spider(self, page):
         # Python: c101280100-p100109 Java: c101280100-p100101
         url = 'https://www.zhipin.com/c101280100-p100101/?page=' + page + '&ka=page-' + page
         headers = {
-            'cookie': 'lastCity=101280100; JSESSIONID=""; __c=1532674186; __g=-; __l=l=%2Fwww.zhipin.com%2F&r=; Hm_lvt_194df3105ad7148dcf2b98a91b5e727a=1531725463,1532505461,1532674186; toUrl=https%3A%2F%2Fwww.zhipin.com%2Fc101280100-p100101%2Fpage%3D10; Hm_lpvt_194df3105ad7148dcf2b98a91b5e727a=1532674604; __a=72893011.1528771525.1532505461.1532674186.53.9.33.45',
-            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36',
+            'user-agent': random.choice(self.user_agent_list)
         }
-        resp = requests.get(url, headers=headers)
+        proxy = {'http': 'http://219.141.153.38:80'}
+        resp = requests.get(url, headers=headers, proxies={'http': 'http://219.141.153.38:80'})
         bs = BeautifulSoup(resp.text, 'html.parser')
         info_primary = bs.find_all(class_='info-primary')
         for info in info_primary:
@@ -67,4 +88,4 @@ if __name__ == '__main__':
     s = Spider()
     for n in range(1, 11):
         s.spider(str(n))
-    s.save_csv()
+    # s.save_csv()

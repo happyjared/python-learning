1b9b3d46cbfede6ef9407547b401ab36a342163f
邱建东 <qiujiandong@talkmoney.cn>
2018-08-07 23:17:32 +0800

    init rds

--- utils/rds.py
+++ utils/rds.py
@@ -1,4 +1,11 @@
 import redis
 
-redis_cli = redis.Redis(host=' 139.199.162.33', port=16937, db=0, password='16937')
-redis_cli.get('a')
+redis_cli = redis.Redis(host='139.199.162.33', port=16937, db=0, password='planet')
+
+print(redis_cli.set('a', 'b'))
+b = redis_cli.get('a').decode('utf-8')
+print(b)
+print(redis_cli.set('c', 1))
+c = redis_cli.get('c').decode('utf-8')
+c = int[c]
+print(c)

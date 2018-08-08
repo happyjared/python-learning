8c3f8d4fbd1d05a5a79c1551758662878717cb31
邱建东 <qiujiandong@talkmoney.cn>
2018-08-09 07:37:31 +0800

    fix

--- single/planet_robot.py
+++ single/planet_robot.py
@@ -7,7 +7,6 @@ from utils import robot
 from planet import Planet
 from datetime import datetime
 
-
 log = logging.getLogger()
 
 
@@ -46,8 +45,8 @@ class PlanetRobot:
                 if not disable_comment:
                     recent_comment = resp['recent_comments'][index]
                     # 无评论或评论列表里没有机器人的回复
-                    if not recent_comment or list(
-                            filter(lambda rc: rc['user_id'] == Planet.my_user_id, recent_comment)):
+                    if not (recent_comment or list(
+                            filter(lambda rc: rc['user_id'] == Planet.my_user_id, recent_comment))):
                         comment_msg = robot.call_text_v1(comment, msg_user_id)
                         self.__robot_comment(msg_id, comment_msg, tl_hash, msg_user_id)
 

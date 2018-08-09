14f6d65ec299ca5997f03f75d04e5f4c4916f8ba
邱建东 <qiujiandong@talkmoney.cn>
2018-08-09 18:50:34 +0800

    update planet

--- single/planet.py
+++ single/planet.py
@@ -1,9 +1,32 @@
-import logger
+import os
+import yaml
+import logging
 import requests
+import logging.config
 from utils import pgs
 from utils import rds
 
-log = logger.setup_logging()
+
+def setup_logging():
+    """日志相关配置
+
+    """
+
+    path = 'logs'
+    if not os.path.exists(path):
+        os.mkdir(path)
+
+    path = '../config/logging.yml'
+    if os.path.exists(path):
+        with open(path, 'r', encoding='utf-8') as f:
+            config = yaml.load(f)
+            logging.config.dictConfig(config)
+    else:
+        logging.basicConfig(level='INFO', filename='info.log',
+                            format='%(asctime)s %(filename)s[%(lineno)d] %(name)s (%(levelname)s): %(message)s')
+
+
+log = logging.getLogger()
 
 
 class Planet(object):

bf974105130d5a0f5645c97f50aeac38fc39bd36
jared <dong1013@foxmail.com>
2018-08-13 13:36:33 +0800

    update pgs.py

--- utils/pgs.py
+++ utils/pgs.py
@@ -7,8 +7,10 @@ log = logging.getLogger()
 class Pgs:
 
     def __init__(self, host='localhost', port=5432, db_name='postgres', user='postgres', password='postgres'):
-        self.conn_info = "host={0} port={1} dbname={2} user={3} password={4}".format(host, port, db_name, user,
-                                                                                     password)
+        conn_info = "host={0} port={1} dbname={2} user={3} password={4}".format(
+            host, port, db_name, user, password)
+        self.conn = psycopg2.connect(conn_info)
+        self.cur = self.conn.cursor()
 
     def handler(self, sql, params):
         """Save or Update or Delete data from PostgreSQL
@@ -20,23 +22,23 @@ class Pgs:
 
         effect_count = 0
         try:
-            conn = psycopg2.connect(self.conn_info)
-            cur = conn.cursor()
-            cur.execute(sql, params)
-            effect_count = cur.rowcount
+            self.cur.execute(sql, params)
+            effect_count = self.cur.rowcount
         except psycopg2.OperationalError:
             log.exception('psycopg2 OperationalError')
             # To close thread
             raise psycopg2.OperationalError
         except psycopg2.IntegrityError:
+            self.cur.rollback()
             # print('psycopg2.IntegrityError.')
             # log.warning('psycopg2.IntegrityError.')
             pass
         except psycopg2.Error:
+            self.conn.rollback()
             log.error('SQL: %s , Params: %s', sql, params)
             log.exception('psycopg2 Error')
         else:
-            conn.commit()
+            self.conn.commit()
         return effect_count
 
     def fetch_all(self, sql, params):
@@ -49,15 +51,16 @@ class Pgs:
 
         rows = None
         try:
-            conn = psycopg2.connect(self.conn_info)
-            cur = conn.cursor()
-            cur.execute(sql, params)
-            rows = cur.fetchall()
+            self.cur.execute(sql, params)
+            rows = self.cur.fetchall()
         except psycopg2.OperationalError:
             log.exception('psycopg2 OperationalError')
             # To close thread
             raise psycopg2.OperationalError
         except psycopg2.Error:
+            self.conn.rollback()
             log.error('SQL: %s , Params: %s', sql, params)
             log.exception('psycopg2 Error')
+        else:
+            self.conn.commit()
         return rows

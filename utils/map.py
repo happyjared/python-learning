30ed69977ffadc5e932db9c336cc513c1f4ca7e5
jared <dong1013@foxmail.com>
2018-08-15 17:02:45 +0800

    update map

--- utils/map.py
+++ utils/map.py
@@ -1,1 +1,20 @@
 import requests
+
+ak = 'TCLfUCrFQDLWQrzz3NKYBwb8ZY57tgAt'
+
+
+def geocoder(address):
+    """ 百度API:地理编码 -> 地址数据转换为坐标点（经纬度）
+        See http://lbsyun.baidu.com/index.php?title=webapi/guide/webservice-geocoding
+    :param address: 地址数据
+    :return: 坐标点（经纬度）
+    """
+
+    api = 'http://api.map.baidu.com/geocoder/v2/?address={0}&output=json&ak={1}'
+    resp = requests.get(api.format(address, ak)).json()
+    status = resp['status']
+    if 0 == status:
+        result = resp['result']
+        location = result['location']
+        lng = location['lng']  # 经度
+        lat = location['lat']  # 纬度

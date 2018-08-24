95b9d56a77f92326c1a3cb5497ae3c60d5e215aa
邱建东 <qiujiandong@talkmoney.cn>
2018-08-24 22:55:19 +0800

    update mobile phone scrapy

--- scrapys/mobilephone/mobilephone/spiders/mp.py
+++ scrapys/mobilephone/mobilephone/spiders/mp.py
@@ -4,8 +4,10 @@ import scrapy
 
 class MpSpider(scrapy.Spider):
     name = 'mp'
-    allowed_domains = ['18183.com']
-    start_urls = ['http://18183.com/']
+    allowed_domains = ['18183.com', 'cnmo.com']
+    # 排行榜 http://top.zol.com.cn/compositor/cell_phone.html
+    # http://detail.zol.com.cn/cell_phone_index/subcate57_list_1.html
+    start_urls = ['http://sj.18183.com/ku', 'http://product.cnmo.com/all/product.html']
 
     def parse(self, response):
         pass

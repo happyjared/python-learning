6904c90fc06f7a987b08dbec22beaf0f6ae59b20
邱建东 <qiujiandong@talkmoney.cn>
2018-08-26 09:40:30 +0800

    update pip

--- scrapys/zealer/zealer/pipelines.py
+++ scrapys/zealer/zealer/pipelines.py
@@ -17,32 +17,29 @@ class ZealerPipeline(object):
     def process_item(self, item, spider):
 
         if isinstance(item, items.MediaItem):
-            series_id = item.get('seriesId')
-            post_id = item.get('postId')
+            series_id, post_id = item.get('seriesId'), item.get('postId')
             key = "zealer:seriesId:{}".format(series_id)
 
             if not self.redis.sismember(key, post_id):
 
-                data = [series_id, post_id]
                 item_field = ['title', 'desc', 'label', 'coverPicture',
                               'mediaInfo', 'commentNum', 'detailUrl', 'liveTime']
-                data.append(item.get(field) for field in item_field)
-                data.append(mytime.now_date())
+                data = [item.get(field) for field in item_field]
+                data.insert(0, series_id), data.insert(1, post_id), data.append(mytime.now_date())
+
                 effect_count = self.postgres.handler(sql.save_media(), tuple(data))
                 if effect_count:
                     self.redis.sadd(key, post_id)
 
         elif isinstance(item, items.CommentItem):
-            post_id = item.get('postId')
-            user_id = item.get('userId')
+            post_id, user_id = item.get('postId'), item.get('userId')
             key = "zealer:postId:{}".format(post_id)
 
             if not self.redis.sismember(key, user_id):
-                data = [post_id]
                 item_field = ['username', 'avatar', 'content', 'commentTime']
-                data.append(item.get(field) for field in item_field)
-                data.append(user_id)
-                data.append(mytime.now_date())
+                data = [item.get(field) for field in item_field]
+                data.insert(0, post_id), data.append(user_id), data.append(mytime.now_date())
+
                 effect_count = self.postgres.handler(sql.save_comment(), tuple(data))
                 if effect_count:
                     self.redis.sadd(key, user_id)

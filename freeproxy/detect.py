76e4e0f53a64df31eb19a2d18a5f4bd15fb62f1b
jared <dong1013@foxmail.com>
2018-08-27 15:33:05 +0800

    upd detect

--- freeproxy/detect.py
+++ freeproxy/detect.py
@@ -8,21 +8,20 @@ redis = app.redis()
 
 
 def handleProxy(keys):
-    """检测redis代理"""
+    """检测代理是否有效"""
 
     for key in keys:
         for member in redis.smembers(key):
             proxy = {key: member}
             if member != checkProxy(proxy, timeout=5):
-                # logging.warning("Handle Proxy %s is invalid", member)
+                logging.warning("Handle Proxy %s is invalid", member)
                 redis.srem(key, member)
 
 
 def cron_handle_proxy():
-    """定时判断Redis代理数据"""
+    """定时检测Redis代理数据"""
 
-    # logging.info("Cron handle proxy")
-    # 对应http和https代理
+    logging.info("Cron handle proxy")
     keys = redis.keys('http*')
     multi_pool = multiprocessing.Pool(len(keys))
     multi_pool.map(handleProxy, keys)

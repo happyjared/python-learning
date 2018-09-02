3af4f64394340724b81a3d190b59097f6c4655e4
邱建东 <qiujiandong@talkmoney.cn>
2018-09-02 20:46:06 +0800

    update imooc

--- scrapys/imooc/imooc/spiders/main.py
+++ scrapys/imooc/imooc/spiders/main.py
@@ -1,7 +1,7 @@
 from scrapy.cmdline import execute
 
 # 免费课程
-execute(['scrapy', 'crawl', 'course'])
+# execute(['scrapy', 'crawl', 'course'])
 # 实战课程
 execute('scrapy crawl coding'.split(' '))
 # 职业路径 TODO

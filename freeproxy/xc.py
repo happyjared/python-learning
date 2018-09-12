5bcbe7f59d9dae5c69fbc6e8906fee4d567bb2bf
邱建东 <qiujiandong@talkmoney.cn>
2018-09-12 22:02:03 +0800

    update xc log

--- freeproxy/xc.py
+++ freeproxy/xc.py
@@ -13,7 +13,8 @@ logging.basicConfig(level='INFO', filename='xc.log',
 xc_index = 'http://www.xicidaili.com/{}/{}'
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
-                  'Chrome/67.0.3396.79 Safari/537.36'
+                  'Chrome/67.0.3396.79 Safari/537.36',
+    'Cookie': '_free_proxy_session=BAh7B0kiD3Nlc3Npb25faWQGOgZFVEkiJWEzYzlmNjYzMGYxMTZlYmUxY2NmOTFjMzM1NTdiODQwBjsAVEkiEF9jc3JmX3Rva2VuBjsARkkiMVltUWw5SUJ6NWx1ZzA5T0pvUGJVNXlKM0FyeG1SUTFVN3Fzdk5vMi9rZVk9BjsARg%3D%3D--174653b72745d641a7e52f0af86e64e731924546; Hm_lvt_0cf76c77469e965d2957f0553e6ecf59=1536759240; Hm_lpvt_0cf76c77469e965d2957f0553e6ecf59=1536760833'
 }
 
 
@@ -48,5 +49,5 @@ def cron_crawl_proxy():
 if __name__ == '__main__':
     # 定时爬取
     days, hours = '0-6', '0-23'
-    cron.cron_blocking(job=cron_crawl_proxy, day_of_week=days, hour=hours, minute='33')
+    cron.cron_blocking(job=cron_crawl_proxy, day_of_week=days, hour=hours, minute='03')
     # cron_crawl_proxy()

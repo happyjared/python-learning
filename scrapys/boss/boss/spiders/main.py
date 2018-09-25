8becd569e04400cf9680f8af2394f6584b878ee8
jared <dong1013@foxmail.com>
2018-09-25 16:27:46 +0800

    update boss main

--- scrapys/boss/boss/spiders/main.py
+++ scrapys/boss/boss/spiders/main.py
@@ -1,3 +1,10 @@
+from utils import cron
 from scrapy.cmdline import execute
 
-execute('scrapy crawl job'.split(' '))
+
+def run():
+    execute('scrapy crawl job'.split(' '))
+
+
+if __name__ == '__main__':
+    cron.cron_blocking(job=run, day_of_week='5')
--- utils/cron.py
+++ utils/cron.py
@@ -6,8 +6,8 @@ cron = 'cron'
 def cron_background(job, day_of_week, hour=0, minute=0):
     """ 后台运行的定时任务
     
-    :param job: 
-    :param day_of_week: 0-6
+    :param job: 任务
+    :param day_of_week: 0-6 (从周一开始)
     :param hour: 0-23
     :param minute: 0-59
     :return: 
@@ -21,7 +21,7 @@ def cron_background(job, day_of_week, hour=0, minute=0):
 def cron_blocking(job, day_of_week, hour=0, minute=0):
     """ 阻塞的定时任务
     
-    :param job: 
+    :param job: 任务
     :param day_of_week: 0-6
     :param hour: 0-23
     :param minute: 0-59

c9ddfba3390abdeb6ced21e864cb70dcb4568930
jared <dong1013@foxmail.com>
2018-09-27 17:30:47 +0800

    update lagou scrapy setting

--- scrapys/lagou/lagou/settings.py
+++ scrapys/lagou/lagou/settings.py
@@ -22,7 +22,7 @@ LOG_LEVEL = 'DEBUG'
 ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-CONCURRENT_REQUESTS = 12
+CONCURRENT_REQUESTS = 5
 
 # Configure a delay for requests for the same website (default: 0)
 # See https://doc.scrapy.org/en/latest/topics/settings.html#download-delay

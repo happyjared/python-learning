3119e840355dbbbb7ea6f00bd12e71a52bcefe79
邱建东 <qiujiandong@talkmoney.cn>
2018-10-12 21:19:06 +0800

    update boss setting

--- scrapys/lagou/lagou/settings.py
+++ scrapys/lagou/lagou/settings.py
@@ -22,12 +22,12 @@ LOG_LEVEL = 'DEBUG'
 ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-CONCURRENT_REQUESTS = 5
+CONCURRENT_REQUESTS = 2
 
 # Configure a delay for requests for the same website (default: 0)
 # See https://doc.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-# DOWNLOAD_DELAY = 1
+DOWNLOAD_DELAY = 2
 # The download delay setting will honor only one of:
 # CONCURRENT_REQUESTS_PER_DOMAIN = 16
 # CONCURRENT_REQUESTS_PER_IP = 12

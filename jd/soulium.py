a042c0d18185896048c6736400980130716e29d8
邱建东 <qiujiandong@talkmoney.cn>
2018-10-24 23:50:49 +0800

    update soul

--- jd/soulium.py
+++ jd/soulium.py
@@ -3,6 +3,15 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+
+def auth():
+    ask = wait.until(EC.presence_of_element_located((By.ID, 'com.android.packageinstaller:id/do_not_ask_checkbox')))
+    ask.click()
+    allow = wait.until(
+        EC.presence_of_element_located((By.ID, 'com.android.packageinstaller:id/permission_allow_button')))
+    allow.click()
+
+
 server = 'http://localhost:4723/wd/hub'
 desired_caps = {
     "platformName": "Android",
@@ -14,18 +23,45 @@ desired_caps = {
 driver = webdriver.Remote(server, desired_caps)
 wait = WebDriverWait(driver, 10)
 
+# 1.动画页
 skip = wait.until(EC.presence_of_element_located((By.ID, 'cn.soulapp.android:id/llSkip')))
 skip.click()
 
+# 2.进入页
 into = wait.until(EC.presence_of_element_located((By.ID, 'cn.soulapp.android:id/btn_newUser')))
 into.click()
 
+# 3.账号页
 phone = wait.until(EC.presence_of_element_located((By.ID, 'cn.soulapp.android:id/etPhone')))
-phone.send_keys('xxx')
+phone.click()
+phone.send_keys('13709641713')
 confirm = wait.until(EC.presence_of_element_located((By.ID, 'cn.soulapp.android:id/rlConfirm')))
 confirm.click()
 
+# 4.密码页
 password = wait.until(EC.presence_of_element_located((By.ID, 'cn.soulapp.android:id/etPwd')))
-password.send_keys('xxx')
+password.click()
+password.send_keys('jdq.soul')
+driver.back()
 login = wait.until(EC.presence_of_element_located((By.ID, 'cn.soulapp.android:id/tvConfirm')))
 login.click()
+
+# 5.授权页
+view = wait.until(EC.presence_of_element_located((By.ID, "cn.soulapp.android:id/textView2")))
+view.click()
+auth()
+view = wait.until(EC.presence_of_element_located((By.ID, "cn.soulapp.android:id/textView3")))
+view.click()
+auth()
+driver.tap([(511, 931)])
+auth()
+driver.tap([(529, 1333)])
+auth()
+
+# 6.广场界面
+square = wait.until(EC.presence_of_element_located((By.ID, "cn.soulapp.android:id/lotSquare")))
+square.click()
+auth()
+driver.tap([(672, 149)])
+while 1:
+    driver.swipe(1000, 1400, 1000, 400, 500)
--- jd/test.py
+++ jd/test.py
@@ -75,51 +75,61 @@ def reqAddress(lat, lng):
         return address
 
 
+def handle(post):
+    post_id = post.get('id')
+    key = "soul:id:{}".format(str(post_id)[-3:])
+    if not redis_cli.sismember(key, post_id):
+        post_type = post.get('type')
+        weather = post.get('weather')
+        author_id = post.get('authorIdEcpt')
+        signature = post.get('signature')
+        come_from = post.get('comeFrom')
+        content = post.get('content')
+        create_time = timestamp_to_datetime(post.get('createTime'))
+        latitude = post.get('latitude')
+        longitude = post.get('longitude')
+        avatar_name = post.get('avatarName')
+        address = ''
+        if latitude:
+            address = reqAddress(latitude, longitude)
+        p = Post(post_id=post_id, post_type=post_type, weather=weather, author_id=author_id,
+                 signature=signature, come_from=come_from, content=content, create_time=create_time,
+                 latitude=latitude, longitude=longitude, address=address, avatar_name=avatar_name)
+        session.add(p)
+        session.commit()
+
+        attachments = post.get('attachments')
+        if attachments:
+            for attachment in attachments:
+                attachment_id = attachment.get('id')
+                attachment_type = attachment.get('type')
+                file_url = attachment.get('fileUrl')
+                file_format = attachment.get('fileFormat')
+                file_width = attachment.get('fileWidth')
+                file_height = attachment.get('fileHeight')
+                file_duration = attachment.get('fileDuration')
+                a = Attachment(attachment_id=attachment_id, attachment_type=attachment_type,
+                               file_url=file_url, file_format=file_format, file_width=file_width,
+                               file_height=file_height, file_duration=file_duration, post_id=post_id)
+                session.add(a)
+                session.commit()
+
+        redis_cli.sadd(key, post_id)
+
+
 def response(flow):
-    if flow.request.pretty_url.startswith("https://api.soulapp.cn/v3/post"):
+    if flow.request.pretty_url.startswith("https://api.soulapp.cn/v3/post/recommended"):
         resp = flow.response.content
         result = json.loads(resp)
         if result.get('success'):
             data = result.get('data')
             post_list = data.get('postList')
             for post in post_list:
-                post_id = post.get('id')
-                key = "soul:id:{}".format(str(post_id)[-3:])
-                if redis_cli.sismember(key, post_id):
-                    continue
-                post_type = post.get('type')
-                weather = post.get('weather')
-                author_id = post.get('authorIdEcpt')
-                signature = post.get('signature')
-                come_from = post.get('comeFrom')
-                content = post.get('content')
-                create_time = timestamp_to_datetime(post.get('createTime'))
-                latitude = post.get('latitude')
-                longitude = post.get('longitude')
-                avatar_name = post.get('avatarName')
-                address = ''
-                if latitude:
-                    address = reqAddress(latitude, longitude)
-                p = Post(post_id=post_id, post_type=post_type, weather=weather, author_id=author_id,
-                         signature=signature, come_from=come_from, content=content, create_time=create_time,
-                         latitude=latitude, longitude=longitude, address=address, avatar_name=avatar_name)
-                session.add(p)
-                session.commit()
-
-                attachments = post.get('attachments')
-                if attachments:
-                    for attachment in attachments:
-                        attachment_id = attachment.get('id')
-                        attachment_type = attachment.get('type')
-                        file_url = attachment.get('fileUrl')
-                        file_format = attachment.get('fileFormat')
-                        file_width = attachment.get('fileWidth')
-                        file_height = attachment.get('fileHeight')
-                        file_duration = attachment.get('fileDuration')
-                        a = Attachment(attachment_id=attachment_id, attachment_type=attachment_type,
-                                       file_url=file_url, file_format=file_format, file_width=file_width,
-                                       file_height=file_height, file_duration=file_duration, post_id=post_id)
-                        session.add(a)
-                        session.commit()
-
-                redis_cli.sadd(key, post_id)
+                handle(post)
+    elif flow.request.pretty_url.startswith("https://api.soulapp.cn/v3/post/recent"):
+        resp = flow.response.content
+        result = json.loads(resp)
+        if result.get('success'):
+            post_list = result.get('data')
+            for post in post_list:
+                handle(post)

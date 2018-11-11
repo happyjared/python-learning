5272096462b5e8f1d3595405d6c2075434296eb7
Jared <dong1013@foxmail.com>
2018-11-11 10:53:22 +0800

    update wx mp

--- wechat/wx_mps.py
+++ wechat/wx_mps.py
@@ -219,11 +219,11 @@ class WxMps(object):
 
 
 if __name__ == '__main__':
-    _id = 5
-    biz = 'MzUxNTA1Mjk5Mg=='
+    _id = 1
+    biz = 'MzU4NjA4NjMwNw=='
     pass_ticket = 'eGQUHVHO2FixN3uybbzK6dMFuWc3BGfX/ehEVzof8UA/H+r3zMSJwWdLuW52e2Hq'
-    app_msg_token = '982_cbsX4H6yIABW86ZedcSlmw1oAaIoDPC4a-CQvw~~'
-    cookie = 'rewardsn=; wxtokenkey=777; wxuin=1604513290; devicetype=Windows8; version=62060426; lang=zh_CN; pass_ticket=eGQUHVHO2FixN3uybbzK6dMFuWc3BGfX/ehEVzof8UA/H+r3zMSJwWdLuW52e2Hq; wap_sid2=CIrci/0FElxmRUZ5NlBkbHBYX052VFRnYUtRcHVIa0RsX3lZWGZaWjh5RkFZV3lzOHh5a1RDaEg4VDRhREFVMUJRYkZXNzlSc0hYbUFwNjZuaGpfWWhEc21BellPdFlEQUFBfjDtmZ7fBTgNQJVO'
+    app_msg_token = '982_qvjD%2FvsgtuR3SNGHm3vjWkgjNsxGpmIOEx0Huw~~'
+    cookie = 'rewardsn=; wxtokenkey=777; wxuin=1604513290; devicetype=Windows8; version=62060426; lang=zh_CN; pass_ticket=eGQUHVHO2FixN3uybbzK6dMFuWc3BGfX/ehEVzof8UA/H+r3zMSJwWdLuW52e2Hq; wap_sid2=CIrci/0FElxPY2lwSFkzYld3RHZaU2daTllOcXRNbDlDX0lTcjBOOE5EdjNrU3FGbUctTWYxcjlKOXp3VlAwR0NfT3Naa3ZYWThfUExHVVpLbk1qZklmNzBFb29ZTllEQUFBfjCcsp7fBTgNQJVO'
     # 以上信息不同公众号每次抓取都需要借助抓包工具做修改
     wxMps = WxMps(_id, biz, pass_ticket, app_msg_token, cookie)
     wxMps.start()  # 开始爬取文章及评论

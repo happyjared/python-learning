b96f5680534297db62c6f2bcb8aa941363acbd19
Jared <dong1013@foxmail.com>
2019-03-23 14:43:10 +0800

    update wx_mps

--- wechat/wx_mps.py
+++ wechat/wx_mps.py
@@ -10,11 +10,11 @@ import requests
 from utils import pgs, es
 
 
-# wx_mps = 'wxmps'  # 这里数据库、用户、密码一致(需替换成实际的)
-# postgres = pgs.Pgs(host='localhost', port='12432', db_name=wx_mps, user=wx_mps, password=wx_mps)
-# elastic = es.Es(host='localhost', port=12900, index='mp', doc='article')
-#
-#
+wx_mps = 'wxmps'  # 这里数据库、用户、密码一致(需替换成实际的)
+postgres = pgs.Pgs(host='localhost', port='12432', db_name=wx_mps, user=wx_mps, password=wx_mps)
+elastic = es.Es(host='localhost', port=12900, index='mp', doc='article')
+
+
 # def load():
 #     for i in range(21, 22):
 #         sql = "select id,mps_biz,last_msg_id,app_msg_token,pass_ticket,wap_sid2 from " \

f5333a75ea6261266a8f34b518519476bb3b9752
Jared <dong1013@foxmail.com>
2019-06-02 11:35:23 +0800

    update jianshu robot

--- spider/jianshu/robot.py
+++ spider/jianshu/robot.py
@@ -30,11 +30,7 @@ logging.basicConfig(level='INFO', filename='info.log',
 # 无头模式
 options = Options()
 options.add_argument('--headless')
-options.add_argument('log-level=3')
-options.add_argument('--no-sandbox')
-options.add_argument('--disable-gpu')
 options.add_argument('window-size=1366x728')
-options.add_argument('--disable-dev-shm-usage')
 driver = webdriver.Chrome(chrome_options=options)
 
 # 登录简书

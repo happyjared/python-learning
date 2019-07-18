eb503f7ebfd8581b12f9aedd2877642c6c9db7c1
Jared <jiandong.qiu@qlchat.com>
2019-07-18 13:42:43 +0800

    update jianshuium

--- spider/jianshu/jianshuium.py
+++ spider/jianshu/jianshuium.py
@@ -1,10 +1,9 @@
 import time
+
 from appium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-
-# Appium Server, 端口默认为4723
+from selenium.webdriver.support.ui import WebDriverWait
 
 server = 'http://localhost:4723/wd/hub'
 desired_capabilities = {
@@ -14,35 +13,100 @@ desired_capabilities = {
     "appActivity": "com.baiji.jianshu.MainActivity"
 }
 
-driver = webdriver.Remote(server, desired_capabilities)
-wait = WebDriverWait(driver, 10)
+data = {"269373": "6"}
+for username, password in data.items():
+    driver = webdriver.Remote(server, desired_capabilities)
+    wait = WebDriverWait(driver, 30)
+    short_wait = WebDriverWait(driver, 10)
+    long_wait = WebDriverWait(driver, 60)
 
-# 点击“我的”
-ele = wait.until(EC.presence_of_element_located((By.ID, 'com.jianshu.haruki:id/tab_mine')))
-ele.click()
-ele = wait.until(EC.presence_of_element_located((By.ID, 'com.jianshu.haruki:id/user_top_info_avatar')))
-ele.click()
+    # 我的
+    ele = wait.until(EC.presence_of_element_located((By.ID, 'com.jianshu.haruki:id/tab_mine')))
+    ele.click()
+    ele = wait.until(EC.presence_of_element_located((By.ID, 'com.jianshu.haruki:id/user_top_info_avatar')))
+    ele.click()
 
-# 登录
-ele = wait.until(EC.presence_of_element_located((By.ID, 'com.jianshu.haruki:id/tv_switch_login_mode')))
-ele.click()
-ele = wait.until(EC.presence_of_element_located((By.ID, 'com.jianshu.haruki:id/et_account')))
-ele.send_keys('')
-ele = wait.until(EC.presence_of_element_located((By.ID, 'com.jianshu.haruki:id/et_verification_code_or_password')))
-ele.send_keys('')
-login = driver.find_element_by_id('com.jianshu.haruki:id/tv_login')
-login.click()
+    # 登录
+    ele = wait.until(EC.presence_of_element_located((By.ID, 'com.jianshu.haruki:id/tv_switch_login_mode')))
+    ele.click()
+    ele = wait.until(EC.presence_of_element_located((By.ID, 'com.jianshu.haruki:id/et_account')))
+    ele.send_keys(username)
+    ele = wait.until(EC.presence_of_element_located((By.ID, 'com.jianshu.haruki:id/et_verification_code_or_password')))
+    ele.send_keys(password)
+    login = driver.find_element_by_id('com.jianshu.haruki:id/tv_login')
+    login.click()
 
-time.sleep(3)
+    # 简书钻 -> 抽奖
+    ele = wait.until(EC.presence_of_element_located((By.ID, 'com.jianshu.haruki:id/tab_jsd')))
+    ele.click()
+    ele = long_wait.until(EC.presence_of_element_located((By.XPATH, '/hierarchy/android.widget.FrameLayout/'
+                                                                    'android.widget.FrameLayout/'
+                                                                    'android.widget.FrameLayout/'
+                                                                    'android.widget.LinearLayout/'
+                                                                    'android.widget.FrameLayout/'
+                                                                    'android.widget.FrameLayout/'
+                                                                    'android.widget.FrameLayout/'
+                                                                    'android.support.v4.view.ViewPager/'
+                                                                    'android.widget.FrameLayout/'
+                                                                    'android.widget.RelativeLayout/'
+                                                                    'android.view.ViewGroup/android.widget.FrameLayout/'
+                                                                    'android.webkit.WebView/android.webkit.WebView/'
+                                                                    'android.view.View[1]/android.view.View[1]/'
+                                                                    'android.view.View[17]/android.view.View[3]')))
+    ele.click()
 
-driver.tap([(535, 1810)])
-time.sleep(10)
-driver.tap([(525, 1530)])
-time.sleep(5)
-while True:
-    driver.tap([(525, 1160)])
-    time.sleep(3)
-    driver.tap([(545, 1020)])
-    time.sleep(35)
-    driver.tap([(990, 100)])
-    time.sleep(3)
+    # 每日抽奖
+    while True:
+        # 1. GO
+        ele = wait.until(EC.presence_of_element_located((By.XPATH, '/hierarchy/android.widget.FrameLayout'
+                                                                   '/android.widget.FrameLayout/'
+                                                                   'android.widget.FrameLayout/'
+                                                                   'android.widget.LinearLayout/'
+                                                                   'android.widget.FrameLayout/'
+                                                                   'android.widget.FrameLayout/'
+                                                                   'android.widget.FrameLayout/'
+                                                                   'android.widget.FrameLayout/'
+                                                                   'android.widget.FrameLayout/'
+                                                                   'android.widget.RelativeLayout/'
+                                                                   'android.view.ViewGroup/'
+                                                                   'android.widget.FrameLayout/android.webkit.WebView/'
+                                                                   'android.webkit.WebView/android.view.View/'
+                                                                   'android.view.View[1]/android.view.View[1]/'
+                                                                   'android.view.View[2]/android.view.View[2]')))
+        ele.click()
+        try:
+            # 2. 继续观看
+            ele = short_wait.until(EC.presence_of_element_located((By.XPATH, '/hierarchy/android.widget.FrameLayout/'
+                                                                             'android.widget.FrameLayout/'
+                                                                             'android.widget.FrameLayout/'
+                                                                             'android.widget.LinearLayout/'
+                                                                             'android.widget.FrameLayout/'
+                                                                             'android.widget.FrameLayout/'
+                                                                             'android.widget.FrameLayout/'
+                                                                             'android.widget.FrameLayout/'
+                                                                             'android.widget.FrameLayout/'
+                                                                             'android.widget.RelativeLayout/'
+                                                                             'android.view.ViewGroup/'
+                                                                             'android.widget.FrameLayout/'
+                                                                             'android.webkit.WebView/'
+                                                                             'android.webkit.WebView/'
+                                                                             'android.view.View/'
+                                                                             'android.view.View[1]/'
+                                                                             'android.view.View[2]/'
+                                                                             'android.view.View[2]/'
+                                                                             'android.view.View[2]')))
+
+        except:
+            pass
+        else:
+            ele.click()
+            # 根据元素是否还可以点击来判断是否还有抽奖次数 todo
+        # 3. 关闭
+        ele = long_wait.until(EC.presence_of_element_located((By.ID, 'com.jianshu.haruki:id/tt_video_ad_close')))
+        ele.click()
+        try:
+            # 4. 知道了 todo
+            time.sleep(5)
+            driver.tap([(540, 1335)])
+        except:
+            pass
